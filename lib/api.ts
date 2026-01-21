b741b1a393e8d772df88ccee63c67439313b6343
AshiqSazid <ashiqsazid494@gmail.com>
2026-01-22 03:38:06 +0600

    fix

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 //export const BASE_URL = "https://api.rx.TheramuseRX.net";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "http://ba-beryl.vercel.app";
 
 export const RECOMMENDATION = `${BASE_URL}/api/recommendations`;
 export const FEEDBACK = `${BASE_URL}/api/feedback`;
